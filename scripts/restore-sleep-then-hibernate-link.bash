7cd2a1570f0a67c265aa140b8766056ddd15f04f
Florian Vick <florian@florian-vick.de>
2020-08-24 12:23:30 +0200

    added comment to restore-sleep-then-hibernate-link

--- scripts/restore-sleep-then-hibernate-link.bash
+++ scripts/restore-sleep-then-hibernate-link.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This script symlinks the systemd-suspend-then-hibernate.service to the systemd-suspend.service
+# GNOME does not allow for a manual selection of 'suspend-then-hibernate',
+# it will invoke 'systemctl suspend' no matter what.
 
 echo "Restoring symlink from systemd-suspend-then-hibernate.service to systemd-suspend.service"
 rm /usr/lib/systemd/system/systemd-suspend.service
